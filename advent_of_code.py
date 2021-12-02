f70fe344aae3be8bd2bca86ba93645523546d5c4
ewaudo <ewaudo@copiakenya.com>
2021-12-02 11:02:57 +0300

    advent_day_one

--- advent_of_code.py
+++ advent_of_code.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-def advent_day_one(filename):
+def advent_day_one_1(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
         lines = list(map(int, lines))
@@ -12,9 +12,25 @@ def advent_day_one(filename):
         return counter
 
 
+def advent_day_one_2(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        lines = list(map(int, lines))
+        counter = 0
+        initial_sum = sum(lines[:3])
+        for i in range(3, len(lines)):
+            tmp_initial_sum = initial_sum + lines[i] - lines[i - 3]
+            print(initial_sum, tmp_initial_sum)
+            if initial_sum < tmp_initial_sum:
+                counter += 1
+            initial_sum = tmp_initial_sum
+        return counter
+
+
 class TestAdventCode(unittest.TestCase):
     def test_day_one(self):
-        self.assertEqual(7, advent_day_one('advent_day_one_test.txt'))
+        self.assertEqual(7, advent_day_one_1('advent_day_one_test.txt'))
+        self.assertEqual(5, advent_day_one_2('advent_day_one_test.txt'))
 
 
 if __name__ == '__main__':
