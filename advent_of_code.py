288e7954f5ef4ebdf8b3c48dacafba897cb18718
ewaudo <ewaudo@copiakenya.com>
2021-12-03 10:14:21 +0300

    advent_of_code dat three

--- advent_of_code.py
+++ advent_of_code.py
@@ -69,15 +69,106 @@ def advent_day_two_2(filename):
         return int(location[0]) * int(location[1])
 
 
-print('advent_day_two_2', advent_day_two_2('advent_day_two.txt'))
+from collections import Counter
 
 
+def get_max(data):
+    if data['0'] > data['1']:
+        return '0'
+    else:
+        return '1'
+
+
+def get_min(data):
+    if data['0'] > data['1']:
+        return '1'
+    else:
+        return '0'
+
+
+def get_oxygen(bit, position, lines):
+    tmp = []
+    for i in lines:
+        i = i.strip()
+        if i[position] == bit:
+            tmp.append(i)
+    return tmp
+
+
+def advent_day_three_1(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        bits = [[] for x in lines[0].strip()]
+        bits_count = []
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            for index, value in enumerate(line):
+                bits[index].append(value)
+
+        for i in bits:
+            bits_count.append(Counter(i))
+
+        gamma = ''.join([get_max(x) for x in bits_count])
+        epsilon = ''.join([get_min(x) for x in bits_count])
+        return int(gamma, 2) * int(epsilon, 2)
+
+
+def advent_day_three_2(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        tmp, index = list(lines), 0
+        while len(tmp) > 1:
+            bits = [[] for x in tmp[0].strip()]
+            for i in range(len(tmp)):
+                line = tmp[i].strip()
+                for _index, value in enumerate(line):
+                    bits[_index].append(value)
+
+            bits_count = Counter(bits[index])
+
+            if bits_count['0'] == bits_count['1'] or bits_count['0'] < bits_count['1']:
+                _bit = '1'
+            else:
+                _bit = '0'
+
+            tmp = get_oxygen(_bit, index, tmp)
+
+            index += 1
+
+        oxygen = ''.join(tmp)
+        tmp, index = list(lines), 0
+        while len(tmp) > 1:
+            bits = [[] for x in tmp[0].strip()]
+            for i in range(len(tmp)):
+                line = tmp[i].strip()
+                for _index, value in enumerate(line):
+                    bits[_index].append(value)
+
+            bits_count = Counter(bits[index])
+
+            if bits_count['0'] == bits_count['1'] or bits_count['0'] < bits_count['1']:
+                _bit = '0'
+            else:
+                _bit = '1'
+
+            tmp = get_oxygen(_bit, index, tmp)
+
+            index += 1
+
+        co_2 = ''.join(tmp)
+        print('co_2', co_2)
+
+        return int(oxygen, 2) * int(co_2, 2)
+
+print('advent_day_three_2', advent_day_three_2('advent_day_three.txt'))
 class TestAdventCode(unittest.TestCase):
     def test_day_one(self):
         self.assertEqual(7, advent_day_one_1('advent_day_one_test.txt'))
         self.assertEqual(5, advent_day_one_2('advent_day_one_test.txt'))
         self.assertEqual(150, advent_day_two_1('advent_day_two_test.txt'))
         self.assertEqual(900, advent_day_two_2('advent_day_two_test.txt'))
+        self.assertEqual(198, advent_day_three_1('advent_day_three_test.txt'))
+        self.assertEqual(230, advent_day_three_2('advent_day_three_test.txt'))
 
 
 if __name__ == '__main__':
